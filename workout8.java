9fab6959218513ecaca5be5249a904796ec0a8db
SANTHOSHKUMAR_SK7 <santhoshsk7077@gmail.com>
2025-03-15 19:39:39 +0530

    Basic_Codes

--- workout8.java
+++ workout8.java
@@ -6,7 +6,7 @@ public class workout8 {
         {
             if(i%2==0)//Prints even number
             {
-                evencount=evencount+1;//Count Totel even number
+                evencount=evencount+1;//Count Total even number
                 System.out.println("EVEN NUMBER:"+i);
             }
             else//Prints odd number
